88e374d69c5a1793566a73b2e6f7bce3fccf7a8f
jamesroach1 <jim.roach18@gmail.com>
2018-03-13 19:03:00 -0400

    added log4j

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -3,10 +3,19 @@ package com.example.websocketdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class WebsocketDemoApplication {
 
 	public static void main(String[] args) {
+
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+			System.setProperty("current.date.time", dateFormat.format(new Date()));
+
+
 		SpringApplication.run(WebsocketDemoApplication.class, args);
 	}
 }
