5046900873d5ea8387cf6a3ce7f786d80c27fa50
jamesroach1 <jim.roach18@gmail.com>
2018-03-13 16:48:17 -0400

    3

--- src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -1,6 +1,9 @@
 package com.example.websocketdemo.controller;
 
 import com.example.websocketdemo.model.ChatMessage;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -15,10 +18,13 @@ public class ChatController {
 	 * /app/chat.sendMessage will be routed to the sendMessage() method
 	 * /app/chat.addUser will be routed to the addUser() method.
 	 */
+
+    static final Logger logger = Logger.getLogger(ChatController.class);
 	
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+       logger.info(chatMessage.getSender() + ", send a message");
         return chatMessage;
     }
 
